ae5331d4ad9f257220cf9eb234db12e518339f20
Frederik Carlier <frederik.carlier@keysight.com>
2023-10-08 23:19:30 +0200

    NSObject: Include <objc/message.h>
    NSObject uses objc_msg_lookup, which is defined in message.h

--- Source/NSObject.m
+++ Source/NSObject.m
@@ -40,6 +40,7 @@
 
 #import "common.h"
 #include <objc/Protocol.h>
+#include <objc/message.h>
 #import "Foundation/NSMethodSignature.h"
 #import "Foundation/NSInvocation.h"
 #import "Foundation/NSLock.h"
